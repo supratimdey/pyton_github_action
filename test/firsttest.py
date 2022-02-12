5d80bba2b9f12accdcec6dfbc778cc155def065c
Supratim Dey <supratimdey@outlook.com>
2022-02-12 19:18:13 +0530

    needed

---
+++ test/firsttest.py
@@ -0,0 +1,6 @@
+ 
+import pytest
+from src.pymain import sdSqr
+
+def test_methond():
+    assert sdSqr(5) == 25 
--- test/pytest.py
+++
@@ -1,5 +0,0 @@
-from numpy import square
-import src.pymain
-
-def test_methond():
-    assert src.pymain.sdSqr(5) == 10 
