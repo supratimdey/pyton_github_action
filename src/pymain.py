c969357ccb7d46729c48022fa93308c69730a4a5
Supratim Dey <supratimdey@outlook.com>
2022-02-12 18:13:04 +0530

    local debug
    local debug

--- src/pymain.py
+++ src/pymain.py
@@ -2,3 +2,6 @@
 import sys
 
 print(sys.version_info.major)
+
+def sdSqr(num):
+    return num * num
---
+++ test/pytest.py
@@ -0,0 +1,5 @@
+from numpy import square
+import src.pymain
+
+def test_methond():
+    assert src.pymain.sdSqr(5) == 10 
